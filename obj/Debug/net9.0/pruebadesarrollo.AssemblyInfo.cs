00120ef94cc9476fe99e65e8bd82f4d3dbba315b
Alan <alan.solano@itq.edu.ec>
2024-11-24 13:07:51 -0500

    taller

--- obj/Debug/net9.0/pruebadesarrollo.AssemblyInfo.cs
+++ obj/Debug/net9.0/pruebadesarrollo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pruebadesarrollo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+772298691c7cb103b34326bcf966085de2cb3c50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744c3f29ef872c13706eb624b5cffff211785d19")]
 [assembly: System.Reflection.AssemblyProductAttribute("pruebadesarrollo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pruebadesarrollo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
